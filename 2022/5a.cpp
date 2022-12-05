3a80aeecc5f36fbbb81a44cf6c3ef39c65f16219
kylechui <kyle.chui+github@pm.me>
2022-12-04 22:36:45 -0800

    feat: Update day 5, part 1 to class-oriented code.

--- 2022/5a.cpp
+++ 2022/5a.cpp
@@ -3,40 +3,65 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-auto getInitialState() {
-    auto line = string{};
-    auto state = vector<string>(9);
-    while (getline(cin, line) && !isdigit(line[1])) {
+class Crane {
+   public:
+    Crane();
+    void moveCrates(const auto& from, const auto& target, const auto& amount);
+    void doInstruction(const auto& instruction);
+    void printTopCrates() const;
+
+   private:
+    vector<string> crates;
+    void moveCrate(const auto& from, const auto& target);
+};
+
+Crane::Crane() {
+    crates = vector<string>(9);
+    for (auto line = string{}; getline(cin, line) && !isdigit(line[1]);) {
         for (size_t i = 1; i < line.size(); i += 4) {
             if (line[i] != ' ') {
-                state[i / 4].push_back(line[i]);
+                crates[i / 4].push_back(line[i]);
             }
         }
     }
-    ranges::for_each(state, [](auto& s) { ranges::reverse(s); });
-    return state;
+    ranges::for_each(crates, [](auto& s) { ranges::reverse(s); });
+}
+
+void Crane::moveCrate(const auto& from, const auto& target) {
+    const auto crate = crates[from].back();
+    crates[target].push_back(crate);
+    crates[from].pop_back();
+}
+
+void Crane::moveCrates(const auto& from, const auto& target,
+                       const auto& amount) {
+    for (auto _ = 0; _ < amount; _++) {
+        moveCrate(from, target);
+    }
+}
+
+void Crane::doInstruction(const auto& instruction) {
+    if (auto match = smatch{}; regex_search(
+            instruction, match, regex("^move (\\d+) from (\\d+) to (\\d+)$"))) {
+        auto amount = stoll(match.str(1));
+        auto from = stoll(match.str(2)) - 1;
+        auto target = stoll(match.str(3)) - 1;
+        moveCrates(from, target, amount);
+    }
+}
+
+void Crane::printTopCrates() const {
+    ranges::for_each(crates, [](const auto& s) {
+        if (!s.empty()) {
+            putchar(s.back());
+        }
+    });
 }
 
 int main() {
-    auto state = getInitialState();
-    auto moveCrates = [&state](auto from, auto target, auto count) {
-        for (auto i = 0; i < count; i++) {
-            state[target].push_back(state[from].back());
-            state[from].pop_back();
-        }
-    };
-
-    auto line = string{};
-    auto match = smatch{};
-    while (getline(cin, line)) {
-        if (regex_search(line, match,
-                         regex("^move (\\d+) from (\\d+) to (\\d+)$"))) {
-            auto count = stoll(match.str(1));
-            auto from = stoll(match.str(2)) - 1;
-            auto target = stoll(match.str(3)) - 1;
-            moveCrates(from, target, count);
-        }
+    auto crane = Crane{};
+    for (auto line = string{}; getline(cin, line);) {
+        crane.doInstruction(line);
     }
-
-    ranges::for_each(state, [](const auto& s) { putchar(s.back()); });
+    crane.printTopCrates();
 }
