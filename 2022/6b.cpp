d09224165f78d3d4f776873fed33f2ac5c4f14fd
kylechui <kyle.chui+github@pm.me>
2022-12-05 21:08:15 -0800

    feat: Finish day 6.

---
+++ 2022/6b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+auto main() -> int {
+    auto line = string{};
+    getline(cin, line);
+
+    const auto numUnique = 4;
+    for (size_t i = 0; i + numUnique < line.size(); i++) {
+        const auto current =
+            unordered_set<char>(line.begin() + i, line.begin() + i + numUnique);
+        if (current.size() == numUnique) {
+            cout << i + numUnique << endl;
+            break;
+        }
+    }
+}
