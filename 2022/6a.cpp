2829dcffec46fefe446124050d3c946a78d0f244
kylechui <kyle.chui+github@pm.me>
2022-12-05 22:03:53 -0800

    feat: Fix constants for day 6.

--- 2022/6a.cpp
+++ 2022/6a.cpp
@@ -7,7 +7,7 @@ auto main() -> int {
     auto line = string{};
     getline(cin, line);
 
-    const auto numUnique = 14;
+    const auto numUnique = 4;
     for (size_t i = 0; i + numUnique < line.size(); i++) {
         const auto current =
             unordered_set<char>(line.begin() + i, line.begin() + i + numUnique);
