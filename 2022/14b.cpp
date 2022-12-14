e9bd501825c75fe5f052a9569e65387975ac999b
kylechui <kyle.chui+github@pm.me>
2022-12-14 13:49:25 -0800

    feat: Use `sregex_iterator` for day 14.

---
+++ 2022/14b.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+auto main() -> int {
+    auto lines = vector<string>{};
+    for (auto line{""s}; getline(cin, line);) {
+        lines.push_back(line);
+    }
+
+    auto rock = set<pair<ll, ll>>{};
+    auto highest = ll{};
+    for (const auto& line : lines) {
+        auto re = regex{"(\\d+),(\\d+)"};
+        auto prevX = optional<ll>{}, prevY = optional<ll>{};
+        for (auto it{sregex_iterator(line.begin(), line.end(), re)};
+             it != sregex_iterator{}; it++) {
+            const auto x = stoll(it->str(1)), y = stoll(it->str(2));
+            if (prevX.has_value() && prevX.value() == x) {
+                for (auto i{min(y, prevY.value())}; i <= max(y, prevY.value());
+                     i++) {
+                    rock.insert({x, i});
+
+                    highest = max(highest, i);
+                }
+            } else if (prevY.has_value() && prevY.value() == y) {
+                for (auto i{min(x, prevX.value())}; i <= max(x, prevX.value());
+                     i++) {
+                    rock.insert({i, y});
+
+                    highest = max(highest, y);
+                }
+            }
+            prevX = x, prevY = y;
+        }
+    }
+
+    constexpr auto start = pair{500ll, 0ll};
+    auto pos{start};
+    auto numSand{0};
+    while (!rock.contains(start)) {
+        if (pos.second == highest + 1) {
+            rock.insert({pos});
+            pos = start;
+            numSand++;
+        } else if (!rock.contains({pos.first, pos.second + 1})) {
+            pos.second++;
+        } else if (!rock.contains({pos.first - 1, pos.second + 1})) {
+            pos.first--;
+        } else if (!rock.contains({pos.first + 1, pos.second + 1})) {
+            pos.first++;
+        } else {
+            rock.insert({pos});
+            pos = start;
+            numSand++;
+        }
+    }
+    cout << numSand << endl;
+}
