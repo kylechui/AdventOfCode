eabd7b10d9dd1104c80f1464fd9d65a18178ef18
kylechui <kyle.chui+github@pm.me>
2022-12-14 12:39:49 -0800

    feat: Update day 13a solution.

--- 2022/13a.cpp
+++ 2022/13a.cpp
@@ -3,37 +3,28 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-auto get_representation(const auto& packet) -> vector<pair<ll, ll>> {
-    auto representation = vector<pair<ll, ll>>{};
-    auto cur = 0, depth = 0;
+auto get_representation(const auto& packet) -> vector<pair<optional<ll>, ll>> {
+    auto representation = vector<pair<optional<ll>, ll>>{};
+    auto cur = optional<ll>{};
+    auto depth = 0ll;
     for (size_t i{0}; i < packet.size(); i++) {
         auto c = packet[i];
         if (isdigit(c)) {
-            cur *= 10;
-            cur += c - '0';
+            cur = 10 * cur.value_or(0ll);
+            cur.value() += c - '0';
         } else {
-            if (isdigit(packet[i - 1])) {
+            if (cur.has_value()) {
                 representation.push_back({cur, depth});
             }
+            cur.reset();
             if (c == ']') {
-                representation.push_back({INT_MIN, depth});
+                representation.push_back({cur, depth});
             }
             depth += c == '[';
             depth -= c == ']';
-            cur = 0;
         }
     }
     return representation;
-}
-
-auto in_right_order(const auto& packets) -> bool {
-    auto leftRep = get_representation(packets[0]),
-         rightRep = get_representation(packets[1]);
-    auto n = min(leftRep.size(), rightRep.size());
-    for (size_t i{0}; i < n; i++) {
-        if (leftRep[i] != rightRep[i]) return leftRep[i] < rightRep[i];
-    }
-    return n == leftRep.size();
 }
 
 auto main() -> int {
@@ -49,7 +40,8 @@ auto main() -> int {
     auto input = vector<vector<string>>(view.begin(), view.end());
     auto sum = 0;
     for (size_t i{0}; i < input.size(); i++) {
-        if (in_right_order(input[i])) {
+        if (get_representation(input[i][0])
+            <= get_representation(input[i][1])) {
             sum += i + 1;
         }
     }
