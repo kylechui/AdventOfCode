6590649a9c454db63924ed090add41756a0432f6
kylechui <kyle.chui+github@pm.me>
2022-12-15 15:43:47 -0800

    feat: Optimize day 15, part a.

--- 2022/15a.cpp
+++ 2022/15a.cpp
@@ -7,6 +7,23 @@ auto get_distance(const auto& p1, const auto& p2) -> ll {
     return abs(p1.first - p2.first) + abs(p1.second - p2.second);
 }
 
+auto merge_ranges(auto& input) -> vector<pair<ll, ll>> {
+    ranges::sort(input, [](const auto& p1, const auto& p2) {
+        if (p1.second == p2.second) return p1.first < p2.first;
+        return p1.second < p2.second;
+    });
+    auto output = vector<pair<ll, ll>>{input.front()};
+    for (const auto& range : input) {
+        if (range.first <= output.back().second + 1) {
+            output.back().first = min(output.back().first, range.first);
+            output.back().second = range.second;
+        } else {
+            input.push_back(range);
+        }
+    }
+    return output;
+}
+
 auto main() -> int {
     auto lines = vector<string>{};
     for (auto line{""s}; getline(cin, line);) {
@@ -14,7 +31,8 @@ auto main() -> int {
     }
 
     constexpr auto row{2'000'000ll};
-    auto positions = unordered_set<ll>{}, beacons = unordered_set<ll>{};
+    auto positions = vector<pair<ll, ll>>{};
+    auto beacons = unordered_set<ll>{};
     for (const auto& line : lines) {
         if (auto match = smatch{};
             regex_match(line, match,
@@ -25,10 +43,17 @@ auto main() -> int {
             auto dist = get_distance(sensor, beacon);
             auto diff = dist - abs(row - sensor.second);
             if (beacon.second == row) beacons.insert(beacon.first);
-            for (auto x{sensor.first - diff}; x <= sensor.first + diff; x++) {
-                positions.insert(x);
+            if (diff > 0) {
+                positions.push_back({sensor.first - diff, sensor.first + diff});
             }
         }
     }
-    cout << positions.size() - beacons.size() << endl;
+
+    auto simplified = merge_ranges(positions);
+    auto total{0ll}, prevStop{LLONG_MIN};
+    for (const auto& [start, stop] : simplified) {
+        total += stop - max(start - 1, prevStop);
+        prevStop = stop;
+    }
+    cout << total - beacons.size() << endl;
 }
