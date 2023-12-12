546629768b4cc990c52b7a556c745946171bd717
Kyle Chui <kyle.chui+github@pm.me>
2023-12-11 18:55:33 -0800

    feat(1b): Only pain.

--- 2023/advent/lib/day1b.ml
+++ 2023/advent/lib/day1b.ml
@@ -12,31 +12,64 @@ let lines fname =
     print_endline ("Error: " ^ err);
     []
 
-let is_digit c = c >= '0' && c <= '9'
+let my_hash = Hashtbl.create 20
 
-let get_first_dig =
-  let get_first_dig' =
-    String.fold_left (fun acc c ->
-        if is_digit acc || not (is_digit c) then acc else c)
-  in
-  get_first_dig' ' '
+let () =
+  Hashtbl.add my_hash "zero" 0;
+  Hashtbl.add my_hash "0" 0;
+  Hashtbl.add my_hash "one" 1;
+  Hashtbl.add my_hash "1" 1;
+  Hashtbl.add my_hash "two" 2;
+  Hashtbl.add my_hash "2" 2;
+  Hashtbl.add my_hash "three" 3;
+  Hashtbl.add my_hash "3" 3;
+  Hashtbl.add my_hash "four" 4;
+  Hashtbl.add my_hash "4" 4;
+  Hashtbl.add my_hash "five" 5;
+  Hashtbl.add my_hash "5" 5;
+  Hashtbl.add my_hash "six" 6;
+  Hashtbl.add my_hash "6" 6;
+  Hashtbl.add my_hash "seven" 7;
+  Hashtbl.add my_hash "7" 7;
+  Hashtbl.add my_hash "eight" 8;
+  Hashtbl.add my_hash "8" 8;
+  Hashtbl.add my_hash "nine" 9;
+  Hashtbl.add my_hash "9" 9
 
-let get_last_dig =
-  let get_last_dig' =
-    String.fold_right (fun c acc ->
-        if is_digit acc || not (is_digit c) then acc else c)
+let contains_at_index str index sub =
+  let sub_len = String.length sub in
+  let str_len = String.length str in
+  if index + sub_len > str_len then false
+  else String.sub str index sub_len = sub
+
+let contains_num_at_index str index =
+  Hashtbl.fold
+    (fun k v r ->
+      match r with
+      | None -> if contains_at_index str index k then Some v else None
+      | Some _ -> r)
+    my_hash None
+
+let get_first_dig s =
+  let rec get_first_dig' str index =
+    match contains_num_at_index str index with
+    | Some num -> num
+    | None -> get_first_dig' str (index + 1)
   in
-  fun line -> get_last_dig' line ' '
+  get_first_dig' s 0
+
+let get_last_dig s =
+  let rec get_last_dig' str index =
+    match contains_num_at_index str index with
+    | Some num -> num
+    | None -> get_last_dig' str (index - 1)
+  in
+  get_last_dig' s (String.length s - 1)
 
 let run fname =
   lines fname
   |> List.map (fun line ->
-         let first_num =
-           line |> get_first_dig |> int_of_char
-           |> Fun.flip ( - ) (Char.code '0')
-         in
-         let last_num =
-           line |> get_last_dig |> int_of_char |> Fun.flip ( - ) (Char.code '0')
-         in
+         let first_num = line |> get_first_dig in
+         let last_num = line |> get_last_dig in
          (10 * first_num) + last_num)
   |> List.fold_left ( + ) 0 |> print_int
